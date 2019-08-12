eb114e045eb7b904390faa4bfc4ca6d0c21ab4f7
SochaJakub <kontakt@quatrodesign.pl>
2019-08-12 16:50:57 +0200

    Update import repository as singleton class.

--- src/Providers/EntitiesServiceProvider.php
+++ src/Providers/EntitiesServiceProvider.php
@@ -15,33 +15,40 @@ namespace Jsocha\Entities\Providers;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class EntitiesServiceProvider
+ *
+ * @package Jsocha\Entities\Providers
+ */
 final class EntitiesServiceProvider extends ServiceProvider
 {
-    /**
-     * Register bindings in the container.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        /** @var Filesystem $filesystem */
-        $filesystem = $this->app->make('files');
-        
-        $path = $filesystem->exists(app_path('Repository')) ? app_path('Repository') : app_path('Repositories');
-        
-        $namespace = $filesystem->exists(app_path('Repository')) ? 'Repository' : 'Repositories';
-        
-        foreach ($filesystem->allFiles($path) as $file) {
-            $realPath = str_replace('/', '\\', $file->getRelativePathname());
-            
-            $className = 'App\\' . $namespace . '\\' . str_replace('.php', '', $realPath);
-            
-            $repository = new $className;
-            
-            $this->app->singleton(get_class($repository), function () use ($repository) {
-                return $repository;
-            });
-        }
-        
-    }
+	/**
+	 * Register bindings in the container.
+	 *
+	 * Przypisuje repozytoria jako singletony do aplikacji, dzieki czemu tylko 1 instancja repozytorium jest zainicjowana w aplikacji
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		/** @var Filesystem $filesystem */
+		$filesystem = $this->app->make('files');
+		
+		$path = $filesystem->exists(app_path('Repository')) ? app_path('Repository') : app_path('Repositories');
+		
+		$namespace = $filesystem->exists(app_path('Repository')) ? 'Repository' : 'Repositories';
+		
+		foreach ($filesystem->allFiles($path) as $file) {
+			$realPath = str_replace('/', '\\', $file->getRelativePathname());
+			
+			$className = 'App\\' . $namespace . '\\' . str_replace('.php', '', $realPath);
+			
+			$repository = new $className;
+			
+			$this->app->singleton(get_class($repository), function () use ($repository) {
+				return $repository;
+			});
+		}
+		
+	}
 }
